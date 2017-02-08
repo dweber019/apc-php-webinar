e92dc1ecb88b51c41c2a1ac6e441c7543a306d49
David Weber <david.weber@w3tec.ch>
2017-02-08 19:05:25 +0100

    Add environment endpoint

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,7 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/environment', function (Request $request) {
+    return response()->json($_ENV);
+});
